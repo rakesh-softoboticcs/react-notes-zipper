67974f10878402774f015cbc124484a044f81c04
rakesh-softoboticcs <rakesh.kp@softobotics.com>
2023-01-01 20:00:06 +0530

    added landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,21 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import LandingPage from './components/screens/LandingPage/LandingPage';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import MyNotes from './components/screens/MyNotes/MyNotes';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header/>
+      <Routes>
+        <Route path='/' element={<LandingPage />}/>
+        <Route path='/mynotes' element={<MyNotes />} />
+      </Routes>
+      <LandingPage/>
       <Footer/>
-    </div>
+    </BrowserRouter>
   );
 }
 
